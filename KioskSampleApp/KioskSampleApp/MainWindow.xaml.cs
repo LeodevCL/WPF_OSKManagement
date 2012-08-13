c243b0dc1e046006d5aae81e259b4a79acded647
Anthony Baker <abakerp@gmail.com>
2012-08-13 16:27:09 +0100

    Adding project files

---
+++ KioskSampleApp/KioskSampleApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KioskSampleApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region event handlers
+
+        private void LaunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            KeyboardManager.LaunchOnScreenKeyboard();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            KeyboardManager.KillOnScreenKeyboard();
+        }
+
+        #endregion
+    }
+}
